e1c2b15986ce2a3c4ead3254becb5368642f19ce
sweetkumar <sweetkumar26.95@gmail.com>
2018-10-12 21:32:01 +0530

    changes done into registration and login

--- config/handlebars-helpers.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  	ifEquals: function(arg1, arg2, options){
-    	return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-  	},
-  	inc: function(arg1, options){
-    	return parseInt(arg1) + 1;
-  	}
-}
-
